f9c6171825da6d1f4f4de5dafad97af37d66ee57
cherylhan <cherylhan@bjtu.edu.cn>
2017-03-09 00:40:20 +0800

    查询后 连接未关闭

--- src/main/java/jh/studio/ctl/AdminDll.java
+++ src/main/java/jh/studio/ctl/AdminDll.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import jh.studio.dal.AdminDal;
 import jh.studio.entity.Admin;
-
+//查询后连接未关闭
 public class AdminDll {
 	public static void main(String[] args) {
 		Admin admin = new Admin(1, "cheryl", "123");
