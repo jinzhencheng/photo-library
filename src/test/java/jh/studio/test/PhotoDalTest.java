f38c22cca3be9ed461678693777e86b5b895e232
cuijianglin <cuijinaglin13@sina.cn>
2017-03-20 09:13:45 +0800

    1. 插入用户收藏图片接口，2. 获取用户收藏图片

---
+++ src/test/java/jh/studio/test/PhotoDalTest.java
@@ -0,0 +1,41 @@
+package jh.studio.test;
+
+import java.util.List;
+
+import jh.studio.dal.PhotoDal;
+import jh.studio.entity.Pagination;
+import jh.studio.entity.Photo;
+import junit.framework.TestCase;
+
+public class PhotoDalTest extends TestCase{
+	
+	public void testGetYear()
+	{
+		PhotoDal pDal = new PhotoDal();
+		List<String> list = pDal.getYear(Pagination.NULL);
+		for(String p:list)
+		{
+			System.out.println(p);
+		}
+	}
+	
+	public void testGetMonth()
+	{
+		PhotoDal pDal = new PhotoDal();
+		List<String> list = pDal.getMonth(Pagination.NULL,"2017");
+		for(String p:list)
+		{
+			System.out.println(p);
+		}
+	}
+	
+	public void testGetPicture()
+	{
+		PhotoDal pDal = new PhotoDal();
+		List<Photo> list = pDal.getPicture(Pagination.NULL,"2017","05");
+		for(Photo p:list)
+		{
+			System.out.println(p.getId());
+		}
+	}
+}
