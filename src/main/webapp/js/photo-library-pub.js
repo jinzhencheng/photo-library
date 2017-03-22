51de9d16d60e4c79bb5a281130d71f82b23ab317
jinzc <jinzc@yiche.com>
2017-03-22 13:38:14 +0800

    修改了category_show_mb.html布局

--- src/main/webapp/js/photo-library-pub.js
+++ src/main/webapp/js/photo-library-pub.js
@@ -10,27 +10,5 @@ $(function(){
 		$(this).addClass("category-nav");
 		$("#show-content").load($(this).attr("link"));
 	});
-	/*
-
-	//根据年月抓取图片
-	$(".month").on("click",function(){
-		var current_year=$(this).parent(".year").attr("value");
-		var current_month=$(this).attr("value");
-		$.ajax({
-			url:"photo!getPicture.action",
-			data:{"year":current_year,"month":current_month},
-			success:function(data){
-				var imgs="";
-				$.each(data,function(index,item){
-					imgs.concat("<img maxPath="+item.path+" src='"+item.minpath+"'/>")
-				});
-				$(this).html(imgs);
-			},
-			error:function(){
-				$.alert("数据获取失败");
-			}
-		});
-	});
-	*/
 
 });
