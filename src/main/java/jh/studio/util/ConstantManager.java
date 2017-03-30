97bba087f0c066f556db721f2eba225b825dc19d
cherylhan <cherylhan@bjtu.edu.cn>
2017-03-30 14:30:54 +0800

    1.修改PhotoAgentDal、添加标签回显功能 2.修改photo_store.html 页面 3.修改struts.xml 配置文件

--- src/main/java/jh/studio/util/ConstantManager.java
+++ src/main/java/jh/studio/util/ConstantManager.java
@@ -3,5 +3,6 @@ package jh.studio.util;
 public class ConstantManager {
 	public static final int ABOUT_FLAG=1;
 	public static final int STATEMENT_FLAG=2;
+	
 	public static final int DEFALT_PHOTO_SHOW_ITEM=30;
 }
